48822fd4fe3b8cdd7e9e33896231e4051fafb857
Rodrigo Martinez <rafamartinezre95@gmail.com>
2026-03-11 17:21:18 -0300

    Se agregaron los inputs y se definieron las preguntas que pide el challenge

---
+++ trivia.py
@@ -0,0 +1,16 @@
+nombre = input ('Ingrese el nombre del jugador:')
+
+print (f'Bienvenido/a {nombre} !!!!')
+
+print ('Pregunta numero 1: ¿Cuanto es la raiz cuadrada de 81?')
+print ('Pregunta numero 2: ¿Cuantas alas tiene un avion?')
+print ('Pregunta numero 3: ¿La tierra es plana o redonda?')
+print ('Pregunta numero 4: ¿Cuantas ceecitas vamos a tomar hoy?')
+
+pregunta_1 = 9
+pregunta_2 = 2
+pregunta_3 = 'plana'
+pregunta_4 = 10
+
+
+
